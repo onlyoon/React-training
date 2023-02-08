6b48c8d52f37f6165808884df9595d92bf406c89
yoon <parkyoon97@gmail.com>
2023-02-08 07:41:01 +0000

    Update ExpenseForm.js and NewExpense.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -70,7 +70,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button>Cancel</button>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="sumbit">Add Expense</button>
       </div>
     </form>
